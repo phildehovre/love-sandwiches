c486c93b1f5c199dc7e6cd2cc02a7ec0585edea3
phildehovre <521153@waes.ac.uk>
2024-03-18 11:22:31 +0000

    Add get sales data + validation

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 import gspread
-import sys, subprocess
 from google.oauth2.service_account import Credentials
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'gspread'])
 
 SCOPE = [
     'https://www.googleapis.com/auth/spreadsheets',
@@ -16,4 +14,40 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 sales = SHEET.worksheet('sales')
 
-print(sales.get_all_values())
+def get_sales_values():
+    """ 
+    Get sales data input from the user
+    """
+
+    while True:
+        print("Please enter the sales data from the last market")
+        print("Data should be six numbers, separated by commas")
+        print("Example: 10,20,30,40,50,60\n")
+
+        data_str = input("Enter your data here: ")
+        values = data_str.split(',')
+
+        validate_data(values)
+        if validate_data(values) == True:
+            print("The data is valid")
+            break
+
+
+
+def validate_data(values):
+    """
+    Makes sure the user input is valid and can be manipulated later
+    """
+
+    try:
+        [int(value) for value in values]
+        if len(values) != 6:
+            raise ValueError(
+                 f"You provided {len(values)} values. 6 are required, please try again.")
+        return True
+    except ValueError as e:
+        print(f"Input error: {e}")
+    return False
+
+
+data = get_sales_values()      
