cfc0093a1e262ed590b90b3dc5ee1d30df398eef
phildehovre <521153@waes.ac.uk>
2024-03-18 11:39:05 +0000

    Add update sheet fn

--- run.py
+++ run.py
@@ -2,52 +2,69 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
-    'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/drive.file',
-    'https://www.googleapis.com/auth/drive'
-]
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT=gspread.authorize(SCOPED_CREDS)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
 
-def get_sales_values():
-    """ 
-    Get sales data input from the user
+def get_sales_data():
     """
-
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
+    """
     while True:
-        print("Please enter the sales data from the last market")
-        print("Data should be six numbers, separated by commas")
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here: ")
-        values = data_str.split(',')
 
-        validate_data(values)
-        if validate_data(values) == True:
-            print("The data is valid")
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
             break
 
+    return sales_data
 
 
 def validate_data(values):
     """
-    Makes sure the user input is valid and can be manipulated later
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
     """
-
     try:
         [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-                 f"You provided {len(values)} values. 6 are required, please try again.")
-        return True
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
     except ValueError as e:
-        print(f"Input error: {e}")
-    return False
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
 
 
-data = get_sales_values()      
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
